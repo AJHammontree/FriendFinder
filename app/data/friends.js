a537036e3581baa5aea7be346dafddaf8f710853
Austin Hammontree <30849233+AJHammontree@users.noreply.github.com>
2018-02-19 10:54:43 -0600

    Final details

---
+++ app/data/friends.js
@@ -0,0 +1,100 @@
+var friends = [
+    {
+        "name": "Elon Musk",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Elon_Musk_2015.jpg/220px-Elon_Musk_2015.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Bill Gates",
+        "photo": "https://pbs.twimg.com/profile_images/929933611754708992/ioSgz49P_400x400.jpg",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Mark Zuckerberg",
+        "photo": "https://specials-images.forbesimg.com/imageserve/59d5062131358e542c034eb7/416x416.jpg?background=000000&cropX1=419&cropX2=1409&cropY1=53&cropY2=1044",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Steve Jobs",
+        "photo": "http://static2.businessinsider.com/image/550c98086bb3f72c337c008b/10-of-the-bravest-moves-steve-jobs-made-at-apple.jpg",
+        "scores": [
+            "5",
+            "2",
+            "3",
+            "3",
+            "1",
+            "5",
+            "2",
+            "1",
+            "4",
+            "4"
+        ]
+    },
+    {
+        "name": "Bruce Lee",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/c/ca/Bruce_Lee_1973.jpg",
+        "scores": [
+            "3",
+            "4",
+            "5",
+            "3",
+            "1",
+            "4",
+            "4",
+            "3",
+            "5",
+            "4"
+        ]
+    },
+    {
+        "name": "Austin",
+        "photo": "https://avatars2.githubusercontent.com/u/30849233?s=400&u=177a28040fd880da1335eee367d72a363321c47a&v=4",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "4",
+            "3",
+            "1",
+            "3",
+            "2",
+            "5",
+            "4"
+        ]
+    }
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+require('./app/routing/htmlRoutes')(app);
+require('./app/routing/apiRoutes')(app);
+
+app.listen(PORT, function() {
+    console.log("[server.js] App listening on PORT " + PORT);
+});
+
+module.exports = app;
